183dadfec3bcf705cbe0cde0e404517c480fa8f2
kactok <104381556+kactok@users.noreply.github.com>
2023-11-05 14:40:54 +0100

    code changes

--- forms.py
+++ forms.py
@@ -1,2 +1,22 @@
-class AddCoffe:
-    pass
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+class AddCoffe(FlaskForm):
+    cafe_name = StringField("Name", validators=[DataRequired()])
+    cafe_rating = SelectField("Rating", choices=[('⭐', '⭐'),
+                                                 ('⭐⭐', '⭐⭐'),
+                                                 ('⭐⭐⭐', '⭐⭐⭐'),
+                                                 ('⭐⭐⭐⭐', '⭐⭐⭐⭐'),
+                                                 ('⭐⭐⭐⭐⭐', '⭐⭐⭐⭐⭐')], validators=[DataRequired()])
+    cafe_location = StringField("Location", validators=[DataRequired()])
+    cafe_summary = CKEditorField("Summary", validators=[DataRequired()])
+    cafe_seats = StringField("Number of seats", validators=[DataRequired()])
+    cafe_wifi = SelectField("Wifi", choices=[('✔️', 'Yes'), ('❌', 'No')], validators=[DataRequired()])
+    cafe_toilet = SelectField("Toilet", choices=[('✔️', 'Yes'), ('❌', 'No')], validators=[DataRequired()])
+    coffee_price = StringField("Coffee price", validators=[DataRequired()])
+    cafe_map_url = StringField("Google Maps URL", validators=[DataRequired(), URL()])
+    cafe_img_url = StringField("Cafe photo URL", validators=[DataRequired(), URL()])
+    submit = SubmitField('Add')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, jsonify, request
 from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
 from forms import AddCoffe
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
@@ -10,6 +11,10 @@ import os
 app = Flask(__name__)
 # 'FLASK_KEY' => your own key as environment variable
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
 # DB_URL -> your own url as environment variable
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
 db = SQLAlchemy()
@@ -26,8 +31,8 @@ class CafeData(db.Model):
     cafe_rating = db.Column(db.String(250), nullable=False)
     cafe_location = db.Column(db.String(250), nullable=False)
     cafe_seats = db.Column(db.String(250), nullable=False)
-    cafe_wifi = db.Column(db.Boolean, nullable=False)
-    cafe_toilet = db.Column(db.Boolean, nullable=False)
+    cafe_wifi = db.Column(db.String(250), nullable=False)
+    cafe_toilet = db.Column(db.String(250), nullable=False)
     coffee_price = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
 
@@ -39,14 +44,42 @@ with app.app_context():
 
 @app.route('/')
 def main():
+    # get data from BD
+    result = db.session.execute(db.select(CafeData))
+    cafes = result.scalars().all()
     """display main page"""
     return render_template('index.html')
 
 
-@app.route('/add')
+@app.route('/add', methods=['POST', 'GET'])
 def add():
     """add new cafe"""
-    pass
+    # pass a form
+    form = AddCoffe()
+    # add new cafe from submit form
+    if form.validate_on_submit():
+        try:
+            new_cafe = CafeData(
+                cafe_name=form.cafe_name.data,
+                cafe_summary=form.cafe_summary.data,
+                cafe_map_url=form.cafe_map_url.data,
+                cafe_img_url=form.cafe_img_url.data,
+                cafe_rating=form.cafe_rating.data,
+                cafe_location=form.cafe_location.data,
+                cafe_seats=form.cafe_seats.data,
+                cafe_wifi=form.cafe_wifi.data,
+                cafe_toilet=form.cafe_toilet.data,
+                coffee_price=form.coffee_price.data,
+                date=str(date.today())
+            )
+            db.session.add(new_cafe)
+            db.session.commit()
+        except IntegrityError:
+            flash('Name already in use.')
+            return render_template('add_coffee.html', add_form=form)
+        else:
+            return redirect(url_for('main'))
+    return render_template('add_coffee.html', add_form=form)
 
 
 @app.route('/delete')
